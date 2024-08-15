060729eb8226cc1512adbc8b0c3c4fa0a0b5c0af
Matheus Reinheimer <mreinheimer0@gmail.com>
2024-08-15 12:05:55 -0300

    chore: add tsup scripts

---
+++ packages/tokens/dist/index.d.ts
@@ -0,0 +1,18 @@
+declare const colors: {
+    white: string;
+    black: string;
+    gray100: string;
+    gray200: string;
+    gray400: string;
+    gray500: string;
+    gray600: string;
+    gray700: string;
+    gray800: string;
+    gray900: string;
+    ignite300: string;
+    ignite500: string;
+    ignite700: string;
+    ignite900: string;
+};
+
+export { colors };
