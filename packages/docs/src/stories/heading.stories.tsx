32cf12c4777c3cb41943a551adec961d721524c1
Matheus Reinheimer <mreinheimer0@gmail.com>
2024-08-20 11:28:34 -0300

    feat: add Heading component on storybook

---
+++ packages/docs/src/stories/heading.stories.tsx
@@ -0,0 +1,27 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom title',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    Children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão o heading sempre será um `h2`, mas você pode mudar para qualquer outro tipo de heading.',
+      },
+    },
+  },
+}
